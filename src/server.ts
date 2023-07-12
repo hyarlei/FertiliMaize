fc82b6bf2840ba728206a57693d7076c518bcf41
hyarlei <hyarleysf@gmail.com>
2023-07-12 11:12:59 -0300

    Initial project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+
+import { routes } from './routes';
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`Tá funcionando CRLH! ${port}`);
+    }
+);
